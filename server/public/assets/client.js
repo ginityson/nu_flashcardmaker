439e0dd216ae5b81e2427f618b511493fed8fc0d
GINI TYSON <ginityson@yahoo.com>
2016-08-25 23:08:45 -0500

    questionable commit maybe an infinate loop

--- server/public/assets/client.js
+++ server/public/assets/client.js
@@ -205,6 +205,26 @@ myApp.controller("PracticeController", ["$scope", "$http",function($scope, $http
       });//end .then
     };//end getDeck()
 
+    $scope.shuffleDeck = function(){
+    console.log( 'in shuffleDeck' );
+    $scope.myCard = shuffle( $scope.myCard );
+  };//end fun shuffleDeck
+
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
     $scope.nextCard = function(){
           console.log(" 10 next clicked");
           $scope.index++;
